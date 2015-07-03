20281396d47a402a192d1f456b0abc98cac776b2
barzhas <pvlkzalga@gmail.com>
2015-07-03 16:16:13 +0800

    added code for q norm calculation

---
+++ modules/mpifourierspacenorms.py
@@ -0,0 +1,46 @@
+import numpy
+import h5py
+import sys,os
+from matplotlib import pyplot as plt
+from mpi4py import MPI
+
+
+comm = MPI.COMM_WORLD
+commSize=comm.Get_size()
+rank= comm.Get_rank()
+
+inputDir='/mnt/cbis/home/barzhas/data/3drec'
+
+
+f=h5py.File(inputDir+"/fcutouts.h5","r")
+qfile=h5py.File(inputDir+"/EucDist","w")
+p1=f["particle1"]
+p2=f["particle2"]
+l=p1.keys()
+m=p2.keys()
+shiftlist=numpy.arange(1,101)
+s=len(shiftlist)
+
+localshift   = [np.array_split(shiftlist, commSize)][rank]
+
+for shift in localshift:
+    stack=numpy.empty((len(l)-shift,63),dtype='float')
+    for i in range(len(l)-shift):
+        print l[i]
+        img=p1[l[i]].value
+        img1=p1[l[i+shift]].value
+        stack[i]=handyCythonLib.qNorm(img,img1)
+    
+    qfile.create_dataset("particle1/"+str(shift), data=stack , compression="gzip", compression_opts=9)
+    tack=numpy.empty((len(m)-shift,63),dtype='float')
+    for i in range(len(m)-shift):
+        print m[i]
+        img=p2[m[i]].value
+        img1=p2[m[i+shift]].value
+        tack[i]=handyCythonLib.qNorm(img,img1)
+
+    qfile.create_dataset("particle2/"+str(shift), data=tack , compression="gzip", compression_opts=9)
+
+f.close()
+qfile.close()
+
